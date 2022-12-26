7f5d136e0580285ea41fe2262fafc25e0a2e7c97
Juyoung4 <47167335+Juyoung4@users.noreply.github.com>
2022-12-27 00:18:19 +0900

    [Solve] ✅ BOJ.20924 트리의 기둥과 가지

---
+++ dfs/BOJ_20924.py
@@ -0,0 +1,48 @@
+from sys import stdin, setrecursionlimit
+from collections import deque
+
+def findLongBranch(graph, Node, visited):
+    if Node not in graph: return 0 # 리프노드
+
+    maxBranch = 0
+    for nextNode, edge in graph[Node].items():
+        if visited[nextNode] == 0: # 다음 방문할 노드가 들린적 없으면
+            visited[nextNode] = 1 #방문흔적 남김
+            branch = edge + findLongBranch(graph, nextNode, visited) # 그 하위 자식노드 들린 branch
+            if branch > maxBranch : maxBranch = branch
+
+    return maxBranch
+
+
+if __name__ == "__main__":
+    N, rootN = map(int, stdin.readline().split()) # 노드개수, 루트노드번호
+    from collections import defaultdict
+
+    graph = defaultdict(dict)
+    
+    # 값 저장
+    for _ in range(N-1):
+        a, b, d = map(int, stdin.readline().split())
+        graph[a][b] = d
+        graph[b][a] = d
+    
+    print(" 노드 인접노드들 : ", graph)
+
+    pillar, branch = 0, 0 # 기둥, 가지
+    rootNB = rootN # 기가노드
+    visited = [0]*(N+1)
+    visited[rootN] = -1
+    while True:
+        if len(graph[rootNB]) > 1 : break
+        adjNode, adjEdge = list(graph[rootNB].items())[0]
+        del graph[adjNode][rootNB]
+        rootNB = adjNode
+        pillar += adjEdge
+        visited[rootNB] = -1  
+    print("### 기둥노드 정보 : ", rootNB, pillar, visited)
+    print("### 그래프 정보 : ", graph)
+
+    branch = findLongBranch(graph, rootNB, visited)
+
+    print("result ; ", pillar, branch)
+    
